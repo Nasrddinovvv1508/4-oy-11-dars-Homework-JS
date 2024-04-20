b3452a1d3cf977913628ccb2ccc51a29755f03ca
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-04-20 11:34:03 +0500

    2nd

--- index.js
+++ index.js
@@ -11,3 +11,20 @@
 // }
 
 // console.log(getInitialOdds(n));
+
+
+        // 2-MASALA
+// let arr = [1, 2, 3, 4, 5];
+
+// function getEvenReverse(arr) {
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 0) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+
+//     return newArr.reverse();
+// }
+
+// console.log(getEvenReverse(arr));
