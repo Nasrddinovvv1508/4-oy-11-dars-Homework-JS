649930dc5e7246f54d9d675b195108e36a6425b8
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-04-20 11:33:20 +0500

    1st

---
+++ index.js
@@ -0,0 +1,13 @@
+        // 1-MASALA
+// let n = 5;
+
+// function getInitialOdds(n) {
+//     let arr = [];
+//     for (let i = 1; arr.length < n; i+=2) {
+//         arr.push(i);
+//     }
+
+//     return arr;
+// }
+
+// console.log(getInitialOdds(n));
