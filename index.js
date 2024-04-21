7f7458fd08a6e6c777598b5b3afa1323eca02f9d
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-04-21 23:47:20 +0500

    [2]: 15th

--- index.js
+++ index.js
@@ -467,3 +467,17 @@
 // }
 
 // console.log(getEvenMax(arr));
+
+
+
+// UY ISHI - 15-masala 
+// let arr = [1, 2, 3, 4, 1, 5, 9, 7];
+// let lMax = arr[0]
+
+// for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
+//                 lMax = arr[i]
+//         }
+// }
+
+// console.log(lMax);
